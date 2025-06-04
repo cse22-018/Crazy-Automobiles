cf5f425e79e9cfffb127cf6159385e27a8686358
cse22-018 <134631928+cse22-018@users.noreply.github.com>
2025-06-04 10:43:41 +0200

    Add files via upload

---
+++ JB Website/ClassController.js
@@ -0,0 +1,15 @@
+import { Script } from 'Script.js';
+import { ScriptOne } from 'ScriptOne.js';
+
+
+	controlClass() {
+		
+		var script = new Script();
+		const scriptOne = new ScriptOne();
+
+		script.enterItems();
+		scriptOne.enterItems();
+		
+	}
+
+	
